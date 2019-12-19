867233817e65c82dfaa8f04de7e9a10ded722be5
malin <348475822@qq.com>
2019-12-19 18:03:45 +0800

    new

--- 08vuex/mogujie/src/pages/Mine.vue
+++ 08vuex/mogujie/src/pages/Mine.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <h1>我的</h1>
-    <input type="file" ref="avatar" multiple />
-    <input type="button" @click="upload" value="上传" />
+    <form class action="/multer/singup" method="post" enctype="multipart/form-data">
+      <!--enctype="multipart/form-data"表示不会对数据本身编码，multer只处理enctype="multipart/form-data"的表单数据-->
+      <input type="file" name="singleFile" value />
+      <input type="submit" name value="上传1" />
+    </form>
+
+    <form class action="/multer/arrup" method="post" enctype="multipart/form-data">
+      <input type="file" name="arrayFile" value multiple />
+      <!--multiple 可上传多张  -->
+      <input type="submit" name value="上传2" />
+    </form>
+
+    <form class action="/multer/fieldup" method="post" enctype="multipart/form-data">
+      <input type="file" name="fieldSingleFile" value />
+      <input type="file" name="fieldArrayFile" value multiple />
+      <input type="submit" name value="上传3" />
+    </form>
   </div>
 </template>
 <script>
